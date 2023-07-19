c7ce67497dec05f191d73db78ac08effc41ae410
GBann <GTBannon@protonmail.com>
2023-07-18 18:22:41 -0600

    added date_created

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 # Pair programmed by Gordon Bannon and Owen Lang
+# Date: 7/18/23
+# Note: View Recipe and Edit routes do not work
 
 
 from flask_app.controllers import controller_recipes, controller_users
